ffea57af48a06dccbbbcbbf37295669cb583e74c
han <95217139+htx996@users.noreply.github.com>
2026-05-18 08:20:25 +0800

    Add constants for nas_monitoring_panel domain

---
+++ custom_components/nas_monitoring_panel/const.py
@@ -0,0 +1,12 @@
+DOMAIN = "nas_monitoring_panel"
+
+CONF_HOST = "host"
+CONF_PORT = "port"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+
+DEFAULT_PORT = 80
+DEFAULT_USERNAME = "admin"
+DEFAULT_NAME = "NAS Monitoring Panel"
+
+PLATFORMS = ["switch"]
