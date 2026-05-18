6fb5b17334607d42e6f48febbf1377832d4b14c1
han <95217139+htx996@users.noreply.github.com>
2026-05-18 08:00:18 +0800

    Add Home Assistant integration constants

---
+++ custom_components/esp8266_nas_monitor/const.py
@@ -0,0 +1,12 @@
+DOMAIN = "esp8266_nas_monitor"
+
+CONF_HOST = "host"
+CONF_PORT = "port"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+
+DEFAULT_PORT = 80
+DEFAULT_USERNAME = "admin"
+DEFAULT_NAME = "NAS Monitor Screen"
+
+PLATFORMS = ["switch"]
